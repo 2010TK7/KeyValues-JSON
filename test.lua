fe381a5ebc39d1711c1b568d0ab41754b91b0f91
TK7 <64648487+2010TK7@users.noreply.github.com>
2020-10-06 19:45:10 +0800

    lua but no JSON.
    cant convert to JSON without extra module now.

---
+++ test.lua
@@ -0,0 +1,4 @@
+require("KeyValues2table")
+KVT.preset("tf_proto_obj_defs_", "zh-hant")
+print(KVT.getvalue("5_1000_field { field_number: 2 }"))
+print(KVT.getvalue("4_25080_field { field_number: 5 }"))
