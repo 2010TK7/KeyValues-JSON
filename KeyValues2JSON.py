d2c44493735d04c4a4fdbdad3da94c7c38ade49c
TK7 <64648487+2010TK7@users.noreply.github.com>
2020-11-10 13:02:33 +0800

    1.3

--- KeyValues2JSON.py
+++ KeyValues2JSON.py
@@ -1,21 +1,26 @@
-import re, sys
 __author__ = "TK7"
-__version__ = "1.2"
+__version__ = "1.3"
 
-def preplan(text, index=0, hardplan=False):
-    text = re.sub("(?<!\:)//.*", "", text)
-    start = text.find('"')
-    ender = max(text.rfind('"'), text.rfind('}'))+1
-    text = text[start: ender]
+def basichardplan(text):
+    import re
     text = re.sub("\"\"\"", r'" ""', text)
     text = re.sub("\"\s+\"(\s+)\"", r'" "^\1"', text)
     text = re.sub("(?<=[\"\{\}])\s+(?=[\"\{\}])", "\r\n", text)
     text = re.sub("\"\^(\s+)\"", r'"\1"', text)
+    return text
+
+def preplan(text, index=1, hardplan=False):
+    import re
+    text = re.sub("(?<!\:)//.*", "", text)
+    start = text.find('"')
+    ender = max(text.rfind('"'), text.rfind('}'))+1
+    text = text[start: ender]
     if type(hardplan) == type(lambda:0):
         text = hardplan(text)
     return [re.split("\r\n", text), text][index]
 
-def convert(array, index=0):
+def convert(text, index=0):
+    import sys
     def _assign(table, key, value):
         checker = False
         if type(value) == list:
@@ -31,31 +36,43 @@ def convert(array, index=0):
         if checker:
             return index
     table = {}
+    charmode = False
     checker = False
-    for i in range(len(array)):
-        if i+index >= len(array):
+    for i in range(len(text)):
+        if i+index == len(text):
             break
-        if array[i+index] == '{':
-            if not checker:
-                print("ERROR: dict for no key.")
-                sys.exit()
-            index = _assign(table, key, convert(array, i+index+1))-i
-            checker = False
-        elif array[i+index] == '}':
-            if checker:
-                print("ERROR: last key no value. (middle table)")
-                sys.exit()
-            return [table, i+index]
-        else:
-            if checker:
-                _assign(table, key, array[i+index][1: -1])
+        if not charmode:
+            if text[i+index] == '"':
+                string = []
+                charmode = True
+            elif text[i+index] == '{':
+                if not checker:
+                    print("ERROR: dict for no key.")
+                    sys.exit()
+                index = _assign(table, key, convert(text, i+index+1))-i
                 checker = False
+            elif text[i+index] == '}':
+                if checker:
+                    print("ERROR: last key no value.")
+                    sys.exit()
+                return [table, i+index]
+        else:
+            if text[i+index] == '"':
+                j = 0
+                while(text[i+index-j-1] == '\\'):
+                    j += 1
+                if j%2:
+                    string.append('"')
+                else:
+                    if not checker:
+                        key = ''.join(string)
+                        checker = True
+                    else:
+                        _assign(table, key, ''.join(string))
+                        checker = False
+                    charmode = False
             else:
-                key = array[i+index][1: -1]
-                checker = True
-    if checker:
-        print("ERROR: last key no value. (main table)")
-        sys.exit()
+                string.append(text[i+index])
     return table
 
 def backer(table, index=0):
@@ -82,7 +99,7 @@ def savejson(text, filename):
     import json
     table = convert(preplan(text))
     with open(filename, "w", encoding="utf-8") as file:
-        json.dump(table, file)
+        json.dump(table, file, ensure_ascii=False)
 
 def undojson(table, filename):
     text = backer(table)
